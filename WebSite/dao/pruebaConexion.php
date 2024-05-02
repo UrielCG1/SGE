bf78b1931d4e034eea2a8a239550406dd4eefe15
ElaRCamo <l22141412@queretaro.tecnm.mx>
2024-05-02 14:38:41 -0600

    Prueba bd

--- WebSite/dao/pruebaConexion.php
+++ WebSite/dao/pruebaConexion.php
@@ -3,11 +3,21 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$conexion = mysqli_connect("127.0.0.1:3306","root","","mydb");
-
+/*$conexion = mysqli_connect("127.0.0.1:3306","root","","mydb");
 if($conexion){
     echo 'Conexión exitosa';
 }else{
     echo 'Conexión fallida';
 }
+*/
+
+    class Connection extends Mysqli{
+        public function __construct(?string $hostname = 'localhost', ?string $username = 'root', ?string $password = '', ?string $database = 'Reminders', ?int $port = 1433)
+        {
+            parent::__construct($hostname, $username, $password, $database, $port);
+            $this->set_charset('utf8');
+            $this->connect_error == NULL ? 'DB Conectada' : die('Error al conectarse a la BD');
+        }
+    }
+
 ?>
