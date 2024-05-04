d773b6cbe9476c1bd0ad6b92e327a335ea51d486
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-05-04 15:56:41 -0600

    update sql file

--- Documentación/db_test-JCA.sql
+++ Documentación/db_test-JCA.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2024 at 11:31 PM
+-- Generation Time: May 04, 2024 at 11:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,21 +34,23 @@ CREATE TABLE `espacios` (
   `tipoEspacio` varchar(50) NOT NULL,
   `disponibilidad` tinyint(1) NOT NULL,
   `ESPACIOScol` varchar(45) NOT NULL,
-  `EVALUACION` int(11) NOT NULL
+  `EVALUACION` int(11) NOT NULL,
+  `ubicacion` varchar(256) DEFAULT NULL,
+  `capacidad` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `espacios`
 --
 
-INSERT INTO `espacios` (`espacioID`, `nombreEspacio`, `descripcion`, `tipoEspacio`, `disponibilidad`, `ESPACIOScol`, `EVALUACION`) VALUES
-(103, 'Salas de conferencias', 'Equipadas con tecnología de audio y video para presentaciones y videoconferencias.\r\n', 'Sala', 1, 'uwu', 10),
-(104, 'Salas de juntas', 'Espacios más pequeños para reuniones de equipos o discusiones más íntimas.\r\n', 'Sala', 1, 'uwu', 10),
-(105, 'Salas de trabajo en equipo', 'Equipadas con pizarras, pantallas compartidas y muebles colaborativos para sesiones de lluvia de ideas y trabajo en grupo.\r\n', 'Sala', 1, 'UWU', 8),
-(106, 'Salas de entrevistas', 'Espacios discretos para entrevistas de trabajo con candidatos.\r\n', 'Sala', 1, 'uwu', 7),
-(107, 'Salas de capacitación', 'Espacios equipados con tecnología audiovisual para sesiones de formación y desarrollo.\r\n', 'Sala', 1, 'xd', 9),
-(108, 'Oficinas', 'Oficinas privadas para trabajadores que necesitan concentración o privacidad. La cual cuentan con monitor y todo el equipo necesario.\r\n', 'Oficina', 1, 'AMLO', 8),
-(109, 'Cubículos', 'Espacios semi-privados con divisiones para trabajadores que necesitan un espacio propio pero no una oficina cerrada.', 'Cubículos', 0, 'lol', 10);
+INSERT INTO `espacios` (`espacioID`, `nombreEspacio`, `descripcion`, `tipoEspacio`, `disponibilidad`, `ESPACIOScol`, `EVALUACION`, `ubicacion`, `capacidad`) VALUES
+(103, 'Salas de conferencias', 'Equipadas con tecnología de audio y video para presentaciones y videoconferencias.\r\n', 'Sala', 1, 'uwu', 10, 'piso 1', 20),
+(104, 'Salas de juntas', 'Espacios más pequeños para reuniones de equipos o discusiones más íntimas.\r\n', 'Sala', 1, 'uwu', 10, 'piso 4', 10),
+(105, 'Salas de trabajo en equipo', 'Equipadas con pizarras, pantallas compartidas y muebles colaborativos para sesiones de lluvia de ideas y trabajo en grupo.\r\n', 'Sala', 1, 'UWU', 8, 'piso 3', 55),
+(106, 'Salas de entrevistas', 'Espacios discretos para entrevistas de trabajo con candidatos.\r\n', 'Sala', 1, 'uwu', 7, 'piso 9', 5),
+(107, 'Salas de capacitación', 'Espacios equipados con tecnología audiovisual para sesiones de formación y desarrollo.\r\n', 'Sala', 1, 'xd', 9, 'piso 2', 25),
+(108, 'Oficinas', 'Oficinas privadas para trabajadores que necesitan concentración o privacidad. La cual cuentan con monitor y todo el equipo necesario.\r\n', 'Oficina', 1, 'AMLO', 8, 'piso 4', 20),
+(109, 'Cubículos', 'Espacios semi-privados con divisiones para trabajadores que necesitan un espacio propio pero no una oficina cerrada.', 'Cubículos', 0, 'lol', 10, 'piso 5', 18);
 
 -- --------------------------------------------------------
 
